a04a401894266abbafa2b6dc55d9d1996070d3d7
Alexa@DESKTOPLabUFRR <alexandrogonsan@outlook.com>
2019-10-30 11:46:10 -0400

    Test Creative-Tim Theme

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,5 @@ Route::get('/', function () {
 Route::get('/', function () {
     return view('en');
 })->name('home');
+Route::get('/', 'UserController@create')->name('home');
 Route::resource('paciente', 'PacienteController');
